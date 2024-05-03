6365fb7517c9fa6ccb1fc8954b379dd1592da95e
g1444332 <sasamaksim10@gmail.com>
2024-05-03 10:04:47 +0300

    init

---
+++ main.py
@@ -0,0 +1,106 @@
+import sys
+import csv
+import matplotlib.pyplot as plt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from design import Ui_MainWindow
+from datetime import datetime
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton_7.clicked.connect(self.open_table_window)
+        self.ui.pushButton_4.clicked.connect(self.show_graph_all)
+        self.ui.pushButton_3.clicked.connect(self.show_graph_next_month)
+        self.canvas = None
+
+    def open_table_window(self):
+        table_window = TableWindow()
+        table_window.load_data_from_csv("data.csv")
+        table_window.exec_()
+
+    def show_graph_all(self):
+        dates = []
+        temperatures = []
+
+        with open("data.csv", "r", newline = "") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                date = datetime.strptime(row['datetime'], '%Y%m%dT%H%M')
+                dates.append(date)
+                temperatures.append(float(row['temperature']))
+
+        # Создание графического холста Matplotlib
+        if self.canvas is None:
+            self.canvas = FigureCanvas(plt.Figure())
+        else:
+            self.canvas.close()
+            self.canvas = FigureCanvas(plt.Figure())
+
+        ax = self.canvas.figure.subplots()
+        ax.plot(dates, temperatures, linestyle = '-')
+        ax.set_xlabel('Дата')
+        ax.set_ylabel('Температура')
+        ax.set_title('Температура за 2 роки')
+        ax.grid(True)
+        self.canvas.draw()
+
+    def show_graph_next_month(self):
+        pass
+
+
+class TableWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Table Window")
+        self.table_view = QtWidgets.QTableView()
+        self.table_view.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.table_view)
+        self.setLayout(self.layout)
+
+    def load_data_from_csv(self, filename):
+        with open(filename, "r", newline = "") as file:
+            reader = csv.reader(file)
+            data = list(reader)
+        self.set_data(data)
+
+    def set_data(self, data):
+        model = QtGui.QStandardItemModel()
+        for row_number, row_data in enumerate(data):
+            if row_number == 0:
+                model.setHorizontalHeaderLabels(row_data)
+            else:
+                items = [QtGui.QStandardItem(field) for field in row_data]
+                model.appendRow(items)
+        self.table_view.setModel(model)
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec_())
+
+#         self.scene = QtWidgets.QGraphicsScene()
+#         self.graphicsView.setScene(self.scene)
+#
+#         self.figure = Figure()
+#         self.axes = self.figure.gca()
+#         self.axes.set_title("My Plot")
+#
+#     def graph(self):
+#         x = [random.randrange(1, 100) for _ in range(10)]
+#         y = [random.randrange(1, 100) for _ in range(10)]
+#
+#         self.axes.clear()
+#         self.axes.plot(x, y, "-k", label = "График внутри виджета QGraphicsView")
+#
+#         self.axes.legend()
+#         self.axes.grid(True)
+#
+#         self.canvas = FigureCanvas(self.figure)
+#         self.proxy_widget = self.scene.addWidget(self.canvas)
+
---
+++ train.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+# Загрузка данных
+data = pd.read_csv('data.csv')
+data['datetime'] = pd.to_datetime(data['datetime'])
+data.set_index('datetime', inplace = True)
+
+# Нормализация данных
+scaler = MinMaxScaler(feature_range = (0, 1))
+data_scaled = scaler.fit_transform(data[['temperature']])
+
+tf.random.set_seed(13)
+
+
+# Функция для создания датасета
+def create_dataset(dataset, look_back = 1):
+    X, Y = [], []
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back), 0]
+        X.append(a)
+        Y.append(dataset[i + look_back, 0])
+    return np.array(X), np.array(Y)
+
+
+# Подготовка данных
+look_back = 1
+train = data_scaled[data.index < pd.Timestamp('20240229T2300')]
+test = data_scaled[data.index >= pd.Timestamp('20240229T2300')]
+
+X_train, Y_train = create_dataset(train, look_back)
+X_test, Y_test = create_dataset(test, look_back)
+
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+# Создание модели LSTM
+model = Sequential()
+model.add(LSTM(50, input_shape = (1, look_back)))
+model.add(Dense(1))
+model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+
+# Обучение модели
+model.fit(X_train, Y_train, epochs = 10, batch_size = 32, verbose = 2)
+
+# Инициализация последнего значения обучающей выборки для начального прогноза
+last_train = X_train[-1].reshape(1, 1, look_back)
+
+# Список для сбора прогнозов
+test_predictions = []
+
+# Делаем прогнозы на количество шагов, равное размеру тестовой выборки
+for _ in range(len(X_test)):
+    current_pred = model.predict(last_train)[0]
+    test_predictions.append(current_pred)
+    # Обновляем last_train, чтобы содержало последний прогноз
+    last_train = np.array([[current_pred]])
+
+# Обратное преобразование для визуализации
+test_predictions = scaler.inverse_transform(test_predictions)
+print(test_predictions)
+print('------------------')
+print(scaler.inverse_transform(test)[:len(test_predictions)])
+
+# Расчет RMSE
+rmse = np.sqrt(mean_squared_error(scaler.inverse_transform(test)[:len(test_predictions)], test_predictions))
+print(f'RMSE: {rmse:.2f}')
+
+# Визуализация
+plt.figure(figsize = (15, 7))
+training_data_to_plot = scaler.inverse_transform(train)[look_back:]
+actual_dates = data.index[data.index >= pd.Timestamp('20240229T2300')]
+plt.plot(data.index[data.index < pd.Timestamp('20240229T2300')][look_back:], training_data_to_plot,
+         label = 'Training Data')
+plt.plot(actual_dates[:len(test_predictions)], scaler.inverse_transform(test)[:len(test_predictions)],
+         label = 'Actual October Data')
+plt.plot(actual_dates[:len(test_predictions)], test_predictions, label = 'Predicted October Data', linestyle = '--')
+plt.title('Comparison of Actual and Predicted Values for October')
+plt.xlabel('Date')
+plt.ylabel('temperature')
+plt.legend()
+plt.show()
