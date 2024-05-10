479a9b10cdcfc59cc4d5ec63ad781b59d303ac5f
g1444332 <sasamaksim10@gmail.com>
2024-05-10 21:50:39 +0300

    update

--- main.py
+++ main.py
@@ -2,9 +2,51 @@ import sys
 import csv
 import matplotlib.pyplot as plt
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QDate
 from design import Ui_MainWindow
 from datetime import datetime
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import train
+
+
+class PlotViewer(QtWidgets.QWidget):
+    def __init__(self, parent = None):
+        super(PlotViewer, self).__init__(parent)
+
+        self.figure = plt.figure(figsize = (16, 5))
+        self.figureCanvas = FigureCanvas(self.figure)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.figureCanvas)
+        self.setLayout(layout)
+
+    def PlotPredict(self, days):
+        self.ax = self.figure.add_subplot()
+        data = train.load_data('data/data_4_years.csv')
+        regressorLSTM = train.load_model("modelLSTM.h5")
+        X_train, y_train, X_test, y_test, scaler, test_data = train.prepare_data(data)
+        forecast_data = train.forecast_temperature(days, regressorLSTM, X_test, scaler)
+        train.plot_forecast(self.ax, days, data, forecast_data, test_data)
+
+    def PlotAll(self):
+        dates = []
+        temperatures = []
+
+        with open("./data/data_day.csv", "r", newline = "") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                date = datetime.strptime(row['datetime'], '%Y%m%dT%H%M')
+                dates.append(date)
+                temperatures.append(float(row['temperatureMean']))
+
+        self.ax = self.figure.add_subplot()
+        self.ax.plot(dates, temperatures, linestyle = '-')
+        self.ax.set_xlabel('Дата')
+        self.ax.set_ylabel('Температура')
+        self.ax.set_title('Графік Температур')
+        self.ax.grid(True)
+
+        self.figureCanvas.show()
 
 
 class Window(QtWidgets.QMainWindow):
@@ -14,42 +56,29 @@ class Window(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.pushButton_7.clicked.connect(self.open_table_window)
         self.ui.pushButton_4.clicked.connect(self.show_graph_all)
-        self.ui.pushButton_3.clicked.connect(self.show_graph_next_month)
-        self.canvas = None
+        self.ui.pushButton_3.clicked.connect(self.show_graph_predict)
+        self.ui.pushButton_6.clicked.connect(self.show_graph_predict)
 
     def open_table_window(self):
         table_window = TableWindow()
-        table_window.load_data_from_csv("data_day.csv")
+        table_window.load_data_from_csv("./data/data_day.csv")
         table_window.exec_()
 
     def show_graph_all(self):
-        dates = []
-        temperatures = []
+        self.plot_viewer = PlotViewer()
+        self.plot_viewer.PlotAll()
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.ui.graphicsView.setScene(self.scene)
+        self.scene.addWidget(self.plot_viewer)
 
-        with open("data/data_day.csv", "r", newline = "") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                date = datetime.strptime(row['datetime'], '%Y%m%dT%H%M')
-                dates.append(date)
-                temperatures.append(float(row['temperature']))
+    def show_graph_predict(self):
+        days = self.ui.date_from.date().daysTo(self.ui.date_to.date())
 
-        # Создание графического холста Matplotlib
-        if self.canvas is None:
-            self.canvas = FigureCanvas(plt.Figure())
-        else:
-            self.canvas.close()
-            self.canvas = FigureCanvas(plt.Figure())
-
-        ax = self.canvas.figure.subplots()
-        ax.plot(dates, temperatures, linestyle = '-')
-        ax.set_xlabel('Дата')
-        ax.set_ylabel('Температура')
-        ax.set_title('Температура за 2 роки')
-        ax.grid(True)
-        self.canvas.draw()
-
-    def show_graph_next_month(self):
-        pass
+        self.plot_viewer = PlotViewer()
+        self.plot_viewer.PlotPredict(days)
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.ui.graphicsView.setScene(self.scene)
+        self.scene.addWidget(self.plot_viewer)
 
 
 class TableWindow(QtWidgets.QDialog):
@@ -83,26 +112,3 @@ app = QtWidgets.QApplication(sys.argv)
 window = Window()
 window.show()
 sys.exit(app.exec_())
-
-
-
-#         self.scene = QtWidgets.QGraphicsScene()
-#         self.graphicsView.setScene(self.scene)
-#
-#         self.figure = Figure()
-#         self.axes = self.figure.gca()
-#         self.axes.set_title("My Plot")
-#
-#     def graph(self):
-#         x = [random.randrange(1, 100) for _ in range(10)]
-#         y = [random.randrange(1, 100) for _ in range(10)]
-#
-#         self.axes.clear()
-#         self.axes.plot(x, y, "-k", label = "График внутри виджета QGraphicsView")
-#
-#         self.axes.legend()
-#         self.axes.grid(True)
-#
-#         self.canvas = FigureCanvas(self.figure)
-#         self.proxy_widget = self.scene.addWidget(self.canvas)
-
--- train.py
+++ train.py
@@ -10,66 +10,68 @@ from keras.optimizers import Adam
 from keras.models import load_model
 
 
-data = pd.read_csv('data/data_4_years.csv')
-data['datetime'] = pd.to_datetime(data['datetime'])
-data.set_index('datetime', inplace=True)
-
-training_data_len = math.ceil(len(data) * .8)
-train_data = data[:training_data_len].iloc[:, :1]
-test_data = data[training_data_len:].iloc[:, :1]
-
-dataset_train = train_data.temperatureMean.values
-dataset_train = np.reshape(dataset_train, (-1, 1))
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_train = scaler.fit_transform(dataset_train)
-
-X_train = []
-y_train = []
-for i in range(50, len(scaled_train)):
-    X_train.append(scaled_train[i - 50:i, 0])
-    y_train.append(scaled_train[i, 0])
-
-X_train, y_train = np.array(X_train), np.array(y_train)
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-
-# regressorLSTM = Sequential()
-# regressorLSTM.add(LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
-# regressorLSTM.add(LSTM(50, return_sequences=False))
-# regressorLSTM.add(Dense(25))
-# regressorLSTM.add(Dense(1))
-#
-# regressorLSTM.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ["accuracy"])
-# regressorLSTM.fit(X_train, y_train, batch_size=1, epochs=12)
-# regressorLSTM.save("modelLSTM.h5")
-regressorLSTM = load_model("modelLSTM.h5")
-
-dataset_test = test_data.temperatureMean.values.reshape(-1, 1)
-scaled_test = scaler.transform(dataset_test)
-
-X_test = []
-y_test = []
-for i in range(50, len(scaled_test)):
-    X_test.append(scaled_test[i - 50:i, 0])
-    y_test.append(scaled_test[i, 0])
-
-X_test, y_test = np.array(X_test), np.array(y_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    data['datetime'] = pd.to_datetime(data['datetime'])
+    data.set_index('datetime', inplace = True)
+    return data
 
 
-last_X = X_test[-1]
-forecast = []
-for _ in range(30):
-    next_pred = regressorLSTM.predict(last_X.reshape(1, -1, 1))
-    forecast.append(next_pred[0, 0])
-    last_X = np.roll(last_X, -1)
-    last_X[-1] = next_pred
+def prepare_data(data):
+    training_data_len = math.ceil(len(data) * .8)
+    train_data = data[:training_data_len].iloc[:, :1]
+    test_data = data[training_data_len:].iloc[:, :1]
 
-plt.figure(figsize=(16, 6))
-plt.plot(data.index, data.temperatureMean, label='Actual Data')
-plt.plot(pd.date_range(test_data.index[-1], periods=30, freq='D'), scaler.inverse_transform(np.array(forecast).reshape(-1, 1)), label='Predict Data', linestyle='--')
-plt.xlabel('Date')
-plt.ylabel('Temperature')
-plt.title('Temperature Predict')
-plt.legend()
-plt.show()
+    dataset_train = train_data.temperatureMean.values
+    dataset_train = np.reshape(dataset_train, (-1, 1))
+
+    scaler = MinMaxScaler(feature_range = (0, 1))
+    scaled_train = scaler.fit_transform(dataset_train)
+
+    X_train = []
+    y_train = []
+    for i in range(50, len(scaled_train)):
+        X_train.append(scaled_train[i - 50:i, 0])
+        y_train.append(scaled_train[i, 0])
+
+    X_train, y_train = np.array(X_train), np.array(y_train)
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+    dataset_test = test_data.temperatureMean.values.reshape(-1, 1)
+    scaled_test = scaler.transform(dataset_test)
+
+    X_test = []
+    y_test = []
+    for i in range(50, len(scaled_test)):
+        X_test.append(scaled_test[i - 50:i, 0])
+        y_test.append(scaled_test[i, 0])
+
+    X_test, y_test = np.array(X_test), np.array(y_test)
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+    return X_train, y_train, X_test, y_test, scaler, test_data
+
+
+def forecast_temperature(days, regressor, X_test, scaler):
+    last_X = X_test[-1]
+    forecast = []
+
+    for _ in range(days):
+        next_pred = regressor.predict(last_X.reshape(1, -1, 1))
+        forecast.append(next_pred[0, 0])
+        last_X = np.roll(last_X, -1)
+        last_X[-1] = next_pred
+
+    forecast_data = scaler.inverse_transform(np.array(forecast).reshape(-1, 1))
+    return forecast_data
+
+
+def plot_forecast(ax, days, data, forecast_data, test_data):
+    ax.plot(data.index, data.temperatureMean, label = 'Actual Data')
+    ax.plot(pd.date_range(test_data.index[-1], periods = days, freq = 'D'), forecast_data, label = 'Predict Data',
+            linestyle = '--')
+    ax.set_xlabel('Дата')
+    ax.set_ylabel('Температура')
+    ax.set_title('Графік Температур')
+    ax.grid(True)
+
